d11746b4590563783e57908b83767c2c1e560774
erasim.zaidi <erasim.zaidi@gmail.com>
2023-01-31 02:00:47 +0530

    deploy-2

--- server/index.js
+++ server/index.js
@@ -1,48 +1,45 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import * as dotenv from 'dotenv'
-dotenv.config()
+/** @format */
 
-import userRoutes from './routes/users.js'
-import questionRoutes from './routes/Questions.js'
-import answerRoutes from './routes/answers.js'
-const app =express()
-app.use(express.json({limit:"30mb", extended:true }))
-app.use(express.urlencoded({limit:"30mb", extended:true }))
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import userRoutes from "./routes/users.js";
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/answers.js";
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get('/', (req, res)=>{
-  res.send("this is the stack overflow api ")
-})
-app.use('/user', userRoutes)
-app.use('/questions', questionRoutes)
-app.use('/answer', answerRoutes)
+app.get("/", (req, res) => {
+	res.send("this is the stack overflow api ");
+});
+app.use("/user", userRoutes);
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
 // app.use('/user/*', (req, res)=>{  res.send("this is the stack overflow api ")})
-const PORT = process.env.PORT || 5000
-const CONNECTION_URL =process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.MONGODB_URI;
 
 // ==========VERCEL DEPLOY================
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
-  })
+if (process.env.NODE_ENV === "production") {
+	app.get("/", (req, res) => {
+		app.use(express.static(path.resolve(__dirname, "../client", "build")));
+		res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+	});
 }
 // ===================
 
-
-
-
-
-
-
-
-
-
-// const CONNECTION_URL ="mongodb+srv://admin:admin@stack-overflow-clone.3mscsti.mongodb.net/stack-overflow-clone?retryWrites=true&w=majority" 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
-.then(()=>app.listen(PORT, ()=>{console.log(`Server is running port ${PORT}`)}))
-.catch((err)=>console.log(err.massage))
+// const CONNECTION_URL ="mongodb+srv://admin:admin@stack-overflow-clone.3mscsti.mongodb.net/stack-overflow-clone?retryWrites=true&w=majority"
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>
+		app.listen(PORT, () => {
+			console.log(`Server is running port ${PORT}`);
+		})
+	)
+	.catch((err) => console.log(err.massage));
